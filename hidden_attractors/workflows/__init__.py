c8a2106225cd752adcfa7d1c17a66aed4889f16a
Xerkkun <xerk.kun@gmail.com>
2026-05-17 17:39:21 -0600

    Add refined basin classification workflow and CLI interface

--- hidden_attractors/workflows/__init__.py
+++ hidden_attractors/workflows/__init__.py
@@ -4,13 +4,17 @@
 from .robustness_overlay import aggregate as aggregate_robustness_overlay
 from .robustness_overlay import launch_independent_jobs as launch_robustness_overlay
 from .robustness_overlay import run_candidate as run_robustness_overlay_candidate
+from .refined_basin import aggregate as aggregate_refined_basin
+from .refined_basin import launch as launch_refined_basin
 from .sphere_controls import aggregate as aggregate_sphere_controls
 from .sphere_controls import launch as launch_sphere_controls
 from .sphere_controls import run_sphere_chunk
 
 __all__ = [
+    "aggregate_refined_basin",
     "aggregate_robustness_overlay",
     "aggregate_sphere_controls",
+    "launch_refined_basin",
     "launch_robustness_overlay",
     "launch_sphere_controls",
     "run_robustness_overlay_candidate",
